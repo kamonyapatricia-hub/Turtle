54bce041e56a6952a8f3af407ab9fe42e3427f50
Hannie <kamonya.patricia@gmail.com>
2026-07-19 16:49:16 +0300

    Add files via upload

---
+++ Turtle2.py
@@ -0,0 +1,25 @@
+import turtle
+#screen
+
+sc =turtle.Screen()
+sc.bgcolor("lightpink")
+sc.title("Draw a square")
+
+#turtle
+
+t=turtle.Turtle()
+
+#Square
+
+t.forward(100)
+t.right(90)
+
+t.forward(100)
+t.right(90)
+
+t.forward(100)
+t.right(90)
+
+t.forward(100)
+
+turtle.done()
---
+++ Turtle_.py
@@ -0,0 +1,13 @@
+import turtle 
+
+print("let's draw using Turtle.")
+
+#Screen 
+
+turtle.Screen().bgcolor("purple")
+
+sc=turtle.Screen()
+sc.setup(400,300)
+turtle.title("Welcome to turtle Window ")
+
+turtle.done()
